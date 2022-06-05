2d6b39cd9211e41a3e4d04d9fce650bd2b8f6928
thaynghiaa <toighetluga@hotmail.com>
2022-06-05 20:35:42 +0700

    import first app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tao mệt quá đừng bắt tao code nữa.
         </p>
         <a
           className="App-link"
